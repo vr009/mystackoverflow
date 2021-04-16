9992089868a4a89f42e43f8d21b2be1354ca1478
vr009 <slavarianov@yandex.ru>
2021-04-17 02:49:27 +0300

    temp commit

--- app/views.py
+++ app/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 
 
+tags = ["favorite language", "the best os", "linux", "microsoft", "github", "docker"]
+
+users = ["dart vader", "obi-wan kenobi", "Palpatine", "Luke skywalker"]
+
 questions = [
     {
         'id': idx,
-        'title': f'Title number {idx}',
+        'tag': tags[idx],
+        'user': users[idx],
+        'title': f'favourite os',
         'text': f'Some text for question #{idx}'
     } for idx in range(4)
-
 ]
+
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'questions': questions})
+
+def hot(request):
+    return render(request, 'hot.html', {'questions': questions})
+
+def onetag(request):
+    return render(request, 'onetag.html', {'questions': questions, 'tags': tags })
+
 
 def ask(request):
     return render(request, 'ask.html', {})
@@ -33,4 +48,4 @@ def tagpage(request):
 
 def one_question(request, pk):
     question = questions[pk]
-    return render(request, 'index.html', {"question": question})
+    return render(request, 'question.html', {"question": question, "users": users})
